79261e47d3855abec0f657405144aa535a1cd6b1
Wojciech Gawronski <afronski@gmail.com>
2015-03-07 09:46:09 +0100

    Adding `dirtree` and additional org-mode Babel enhancements: `ditaa` and `plantuml`.

--- personal/customizations-14-various.el
+++ personal/customizations-14-various.el
@@ -1,3 +1,10 @@
 ; Changing default browser.
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "chromium")
+
+(setq org-plantuml-jar-path (expand-file-name "/opt/plantuml/plantuml.jar"))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((plantuml . t)
+   (ditaa . t)))
---
+++ personal/customizations-15-dirtree.el
@@ -0,0 +1,4 @@
+(require 'dirtree)
+
+(autoload 'dirtree "dirtree" "Add directory to tree view" t)
+(global-set-key "\C-o" 'dirtree-show)
