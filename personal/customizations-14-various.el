93b3cc0bafed472b472c5d9c0db2ce8086c6f8d1
Wojciech Gawronski <afronski@gmail.com>
2015-04-11 10:10:24 +0200

    Adding neotree instead dirtree, and settings for web development.

--- personal/customizations-14-various.el
+++ personal/customizations-14-various.el
@@ -4,6 +4,13 @@
 
 (setq org-plantuml-jar-path (expand-file-name "/opt/plantuml/plantuml.jar"))
 
+(setq js-indent-level 2)
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-indent-style 2)
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((plantuml . t)
--- personal/customizations-15-dirtree.el
+++
@@ -1,4 +0,0 @@
-(require 'dirtree)
-
-(autoload 'dirtree "dirtree" "Add directory to tree view" t)
-(global-set-key "\C-o" 'dirtree-show)
